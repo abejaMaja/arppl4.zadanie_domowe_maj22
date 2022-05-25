51d00dbaa45fea11e6d1804f031a94408cff2ce2
Maja <m.remizowicz@gmail.com>
2022-05-25 11:19:06 +0200

    Update Zadanie2.java

--- src/main/java/pl/sda/arp4/zadanie_samolot/Zadanie2.java
+++ src/main/java/pl/sda/arp4/zadanie_samolot/Zadanie2.java
@@ -1,1 +1,45 @@
+package pl.sda.arp4.zadanie_samolot.zadanie2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Zadanie33 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String miejsce = "";
+        boolean stan = true;
+      
+       while (stan) {
+
+            System.out.println("Podja kod siedzenia");
+            miejsce = scanner.next();
+            List <String> itemList = Arrays.asList("ABCDEF".split(""));
+            List<String> list = Arrays.asList(miejsce.split(""));
+                            
+            if (miejsce.length()!=2){
+                System.out.println("Kod musi zawierać dokładnie dwa znaki");
+            }else if(!isContainsItemNumbersFromList(itemList, list)){
+                System.out.println("Pierwasza litera kodu musi zawierac duza litere z zakresu A-F");
+            }else{
+                System.out.println("Dziękujemy za podanie miejsca, podałeś " + miejsce);
+                stan = false;
+            }
+         
+        }
+       
+    }
+    
+        public static boolean isContainsItemNumbersFromList(List<String> items, List<String> inputStr)
+        {
+               
+            if(items.contains(inputStr.get(0)))
+            {
+              return true;
+            }
+           
+            return false;
+        }
+    
+}
 
